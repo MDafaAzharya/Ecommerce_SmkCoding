697ec8937e8aabe8cbbc5741887064a9828199fe
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-10-27 14:43:58 +0700

    Pertemuan 4 - SMK Coding

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -13,6 +13,8 @@ const allProducts = computed(() => {
     <div class="container">
       <div class="py-10">
         <div class="mb-6 flex justify-end gap-6">
+          <NuxtLink to="/category/create" class="bg-orange-500 text-white flex justify-
+          center items-center px-3 rounded-lg">Create Category</NuxtLink>
           <Dropdown @selected-category="selectedCategory = $event" />
         </div>
         <div class="flex gap-6 flex-wrap mx-auto">
